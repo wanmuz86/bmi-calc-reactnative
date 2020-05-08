b8b05222575848cbafc0c3b5858fc17682e15267
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-08 21:48:11 +0800

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableHighlight,Keyboard,Touchable } from 'react-native';
 
 export default function App() {
+  const [height,setHeight] = useState(0)
+  const [weight,setWeight] = useState(0)
+  const [bmi, setBmi] = useState(0)
+  const [indicator, setIndicator] = useState('')
+  const calculateBmi = () => {
+    let bmic = weight / ((height/100)**2)
+    setBmi(bmic.toFixed(1))
+    if (bmic < 18){
+      setIndicator("Underweight")
+    }
+    else if (bmic < 25){
+      setIndicator("Normal")
+    }
+    else if (bmic < 30){
+      setIndicator("Overweight")
+    }
+    else {
+      setIndicator("Obese")
+    }
+  }
+
   return (
+   <TouchableHighlight onPress={Keyboard.dismiss}>
+    <ImageBackground source={require('./assets/bg.png') }style={{width:'100%', height:'100%'}}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headingText}>BMI Calculator</Text>
+  
+  {/* Almost similar to div class row */}
+    <View style={{flexDirection:'row'}}>
+      <TextInput 
+      placeholder="Height"
+      keyboardType="numeric"
+      style={styles.textInputStyle}
+      value={height}
+      onChangeText={text=> setHeight(text)}
+      />
+      <TextInput 
+      placeholder="Weight"
+      keyboardType="numeric"
+      style={styles.textInputStyle}
+      value={weight}
+      onChangeText={text=>setWeight(text)}
+      />
     </View>
+    <TouchableHighlight onPress={calculateBmi}>
+      <Text style={{textAlign:'center',fontSize:30,color:'orange',padding:30}}>Calculate</Text>
+    </TouchableHighlight>
+  <Text style={styles.textStyle}>{bmi}</Text>
+    <Text style={styles.textStyle}>{indicator}</Text>
+    </View>
+    </ImageBackground>
+    </TouchableHighlight>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+  headingText: {
+    color:'yellow',
+    marginTop:30,
+    textAlign:'center',
+    fontSize:30
+    
+  },
+  textInputStyle:{
+    height:80,
+    textAlign:"center",
+    width:'50%',
+    fontSize:50,
+    marginTop:24,
+    color:'yellow'
+  },
+  textStyle: {textAlign:'center',fontSize:30,color:'yellow',padding:30}
 });
